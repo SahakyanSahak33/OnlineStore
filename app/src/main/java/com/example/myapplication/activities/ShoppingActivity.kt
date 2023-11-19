5a23c625641581307f9944f1bf6723afc28a4351
SahakyanSahak33 <sahakyansahak303@gmail.com>
2023-11-19 16:02:46 +0400

    Adding new classes and new packages

---
+++ app/src/main/java/com/example/myapplication/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
